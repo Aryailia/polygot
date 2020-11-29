f978b1acd65ecb66342e1fdf3d95a53e1b1b49c3
Aryailia <Aryailia@users.noreply.github.com>
2020-11-30 04:19:39 +0800

    refactor splitter for clarity and speed

--- rust/post.rs
+++ rust/post.rs
@@ -1,8 +1,9 @@
 use crate::custom_errors::ParseError;
 use crate::helpers::parse_tags_and_push;
-use crate::traits::{BoolExt, RStr, RangeExt, VecExt};
+use crate::traits::{BoolExt, VecExt};
 use std::borrow::Cow;
-use std::mem::replace;
+
+//run: cargo test -- --nocapture
 
 const API_SET_LANGUAGE: &str = "api_set_lang:";
 const ALL_LANG: Option<&str> = None;
@@ -27,67 +28,84 @@ pub struct PostView<'a> {
 
 impl<'a> Post<'a> {
     pub fn new(text: &'a str, comment_marker: &str) -> Result<Self, ParseError<'a>> {
-        // 'lang_max_count' will count all duplicates (which is the common case)
+        // 'lang_count' will count all duplicates (which is the common case)
         // e.g. api_set_lang: en jp
         //      api_set_lang: ALL
         //      api_set_lang: en
         // counts four
         // It might just be better to let 'unique_langs' auto size
-        let (section_count, lang_max_count) = text
-            .lines()
-            // TODO: rename find_config_key_end
-            .filter_map(|line| find_config_key_end(line, comment_marker))
-            .map(|body| body.split_whitespace().count())
-            .fold((1, 0), |(count, sum), to_add| (count + 1, sum + to_add));
+        let (part_count, lang_max) = SplitByLabel::new(text, comment_marker).fold(
+            (0, 0),
+            |(sections, langs), (_, lang_label, _, _)| {
+                (sections + 1, langs + lang_label.split_whitespace().count())
+            },
+        );
 
-        let mut unique_langs = Vec::with_capacity(lang_max_count);
-        let mut parts = Vec::with_capacity(section_count);
-        let mut toadd_lang = None;
-        let mut toadd_rstr = 0..0;
-        for (row, (range, _)) in (0..text.len()).split(text).enumerate() {
-            let line = range.of(text);
-            if let Some(lang_str) = find_config_key_end(line, comment_marker) {
+        let mut unique_langs = Vec::with_capacity(lang_max);
+        let mut parts = Vec::with_capacity(part_count);
+        let mut labels = Vec::with_capacity(part_count);
+        {
+            let mut cur_langs = ALL_LANG_REPR[0]; // start with an all label
+            for (section, next_langs, line, row) in SplitByLabel::new(text, comment_marker) {
+                let has_all_label = cur_langs
+                    .split_whitespace()
+                    .any(|t| ALL_LANG_REPR.contains(&t));
+
+                // Build up 'unique_langs' list
                 // e.g. 'api_set_lang: en ALL jp' -> 'en all jp'
                 // May be both all and list languages
-                let has_all_tag = line.split_whitespace().any(|t| ALL_LANG_REPR.contains(&t));
-                let langs = parse_tags_and_push(&mut unique_langs, lang_str, &ALL_LANG_REPR, false)
-                    .map_err(|err| (row, line, Cow::Owned(err)))?;
+                let langs =
+                    parse_tags_and_push(&mut unique_langs, cur_langs, &ALL_LANG_REPR, false)
+                        .map_err(|err| (row, line, Cow::Owned(err)))?;
 
-                let langs = (!has_all_tag && !langs.is_empty()).to_some(langs);
-                parts.push_and_check((
-                    replace(&mut toadd_lang, langs),
-                    replace(&mut toadd_rstr, range.end..range.end),
-                ));
-            } else {
-                toadd_rstr.expand(range.end);
+                //print!("{:?} | {:?}", cur_langs, langs);
+                let label = (!has_all_label && !langs.is_empty()).to_some(langs);
+                //println!(" {:?} | {:?}", label, section);
+
+                labels.push_and_check(label);
+                parts.push_and_check(section);
+                cur_langs = next_langs;
+
+                //println!("{:?}", replace(&mut label, Some(langs)));
+                //println!("{:?}", has_all_label);
+                //println!("{:?}\n", section);
             }
         }
-        parts.push_and_check((toadd_lang, toadd_rstr));
+
+        //for i in 0..part_count {
+        //    println!("{:?}\n{:?}\n", labels[i], parts[i]);
+        //}
+        //println!();
 
         unique_langs.sort_unstable();
 
         // Transpose 'parts' from by parts-by-langs to langs-by-parts (PostView)
-        let lang_count = unique_langs.len();
+        let lang_count = unique_langs.len(); // bet
         let mut view_list = Vec::with_capacity(std::cmp::max(lang_count, 1));
         if unique_langs.is_empty() {
-            // For only all lang case, still filter out api_set_lang markup
-            // i.e. use 'parts' not the original 'text'
-            let mut parts_to_str = Vec::with_capacity(parts.len());
-            parts_to_str.extend(parts.iter().map(|(_, range)| range.of(text)));
             view_list.push_and_check(PostView {
                 lang: None,
-                body: parts_to_str,
+                body: parts,
             });
         } else {
             for lang in &unique_langs {
-                let size = parts.iter().filter_map(|x| pick_lang(x, lang)).count();
-                let mut view = Vec::with_capacity(size);
-                view.extend(
-                    parts
-                        .iter()
-                        .filter_map(|x| pick_lang(x, lang))
-                        .map(|r| r.of(text)),
-                );
+                let mut view = Vec::with_capacity(part_count);
+
+                let mut iter = labels.iter();
+                view.extend(parts.iter().filter(|_| {
+                    if let Some(Some(lang_labels)) = iter.next() {
+                        //println!("{:?}", lang_labels);
+                        //false
+                        lang_labels.iter().any(|l| l == lang)
+                    } else {
+                        true
+                    }
+                }));
+                //view.extend(
+                //    parts
+                //        .iter()
+                //        .filter_map(|x| pick_lang(x, lang))
+                //);
 
                 view_list.push_and_check(PostView {
                     lang: Some(lang),
@@ -107,28 +125,137 @@ impl<'a> Post<'a> {
 /******************************************************************************
  * Post helper functions
  ******************************************************************************/
-fn pick_lang(entry: &(Option<Vec<&str>>, RStr), pick: &str) -> Option<RStr> {
-    let (maybe_all_langs, range) = entry;
-    if let Some(lang_list) = maybe_all_langs {
-        if lang_list.iter().all(|lang| *lang != pick) {
-            return None;
+struct SplitByLabel<'a, 'b> {
+    iter: std::str::Chars<'a>,
+    comment: &'b str,
+    comment_len: usize,
+    row: usize,
+}
+impl<'a, 'b> SplitByLabel<'a, 'b> {
+    fn new(buffer: &'a str, comment: &'b str) -> Self {
+        Self {
+            iter: buffer.chars(),
+            comment,
+            comment_len: comment.len(),
+            row: 0,
         }
     }
-    Some(range.start..range.end) // duplicate because access by reference
 }
 
-fn is_all_lang(tag: &str) -> bool {
-    tag == "*" || tag == "ALL"
-}
+impl<'a, 'b> Iterator for SplitByLabel<'a, 'b> {
+    type Item = (&'a str, &'a str, &'a str, usize);
+    fn next(&mut self) -> Option<Self::Item> {
+        enum State {
+            Blank,
+            Newline,
+            Slash(usize),
+        }
+        let mut state = State::Newline; // \n needs not preceed first char
+        let mut cursor = 0;
+        let as_str = self.iter.as_str();
+        while let Some(c) = self.iter.next() {
+            let rest = &as_str[cursor..];
+            let is_newline = c == '\n';
+            if is_newline {
+                self.row += 1;
+            }
 
-#[inline]
-fn find_config_key_end<'a>(line: &'a str, comment_marker: &str) -> Option<&'a str> {
-    let trimmed = line.trim_start();
-    if trimmed.starts_with(comment_marker) {
-        let comment_body = trimmed[comment_marker.len()..].trim();
-        if comment_body.starts_with(API_SET_LANGUAGE) {
-            return Some(&comment_body[API_SET_LANGUAGE.len()..]);
+            match (&state, is_newline) {
+                (State::Newline, _) if rest.starts_with(self.comment) => {
+                    state = State::Slash(cursor); // after newline
+
+                    // Skipping count - 1 because 'while' walk already skips one
+                    for _ in 1..self.comment_len {
+                        cursor += c.len_utf8();
+                        self.iter.next();
+                    }
+                }
+                (State::Slash(_), _) if c.is_whitespace() => {}
+                (State::Slash(mid), _) => {
+                    //let right = &as_str[*mid..];
+                    //let end = right.find('\n').unwrap_or(right.len());
+                    //return Some((&as_str[..*mid], &right[..end]));
+                    //println!("- {:?}\n", &as_str[*mid..]);
+
+                    if rest.starts_with(API_SET_LANGUAGE) {
+                        let left = &as_str[..*mid];
+
+                        // Skip until newline
+                        let mut end = cursor + c.len_utf8();
+                        while let Some(r) = self.iter.next() {
+                            end += r.len_utf8();
+                            if r == '\n' {
+                                break;
+                            }
+                        }
+                        let right = &as_str[cursor + API_SET_LANGUAGE.len()..end];
+                        let label = &as_str[*mid..end];
+
+                        return Some((left, right, label, self.row));
+                    } else {
+                        state = State::Blank;
+                    }
+                }
+                (_, true) => state = State::Newline,
+                (_, false) => state = State::Blank,
+            }
+
+            cursor += c.len_utf8();
+        }
+        if cursor > 0 {
+            Some((&as_str[..cursor], "", "", self.row))
+        } else {
+            None
         }
     }
-    None
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    const COMMENT: &str = "//";
+
+    #[test]
+    fn integration_test() {
+        //let content = std::fs::read_to_string("rust/test.adoc").unwrap();
+        //let post = Post::new(content.as_str(), "//").unwrap();
+
+        //println!("{:#?}", post.views);
+    }
+
+    fn parse(buffer: &str) -> Vec<(&str, &str)> {
+        SplitByLabel::new(buffer, COMMENT)
+            .map(|x| (x.0, x.1))
+            .collect::<Vec<_>>()
+    }
+
+    #[test]
+    fn split_only_delimiter() {
+        let line = &format!("{}{} hello", COMMENT, API_SET_LANGUAGE);
+        assert_eq!(vec![("", " hello")], parse(line));
+    }
+    #[test]
+    fn split_delimiter_start() {
+        let line = &format!("{}{} hello\nasdf\n", COMMENT, API_SET_LANGUAGE);
+        assert_eq!(vec![("", " hello\n"), ("asdf\n", "")], parse(line));
+    }
+    #[test]
+    fn split_delimiter_end() {
+        let line = &format!("stuff and things\n{}{} hello", COMMENT, API_SET_LANGUAGE);
+        assert_eq!(vec![("stuff and things\n", " hello")], parse(line));
+    }
+    #[test]
+    fn split_empty() {
+        let against: Vec<(&str, &str)> = Vec::new();
+        assert_eq!(against, parse(""));
+    }
+    #[test]
+    fn split_no_delimiter() {
+        let line = "lorem ipsem
+// adsfkj
+qwekrjl
+qewrkjladjsf ldjas lf
+";
+        assert_eq!(vec![(line, "")], parse(line));
+    }
 }
